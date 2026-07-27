25a5b9da12f09367aff7439322738dccd98557e1
Han-Donghee <oneeasthee@gmail.com>
2026-07-27 13:30:40 +0900

    주석 오타 수정

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def run_pipeline(file_path: str):
     media = load_media(file_path) # 입력된 파일 분석
     print(f"[입력 처리] media_type={media['media_type']}") # 입력된 파일이 영상 또는 오디오인지 확인하기 위해 출력
     print(f"  audio_path: {media['audio_path']}") # Whisper가 사용할 오디오 위치 출력
-    print(f"  video_path: {media['video_path']}") # OpenCV가 사용할 영상 위치 출
+    print(f"  video_path: {media['video_path']}") # OpenCV가 사용할 영상 위치 출력
 
     # 아직 구현하지 않은 기능
     # TODO: media["audio_path"] -> Whisper.py 로 STT + 타임스탬프/신뢰도 추출
@@ -16,4 +16,4 @@ def run_pipeline(file_path: str):
 
 
 if __name__ == "__main__": # 이 파일을 직접 실행했을 때만 아래 코드 실행
-    run_pipeline("lecture_test4.mp4") # 테스트용으로 lecture_teat4.mp4를 입력하여 파이프라인이 정상적으로 동작하는지 확
+    run_pipeline("lecture_test4.mp4") # 테스트용으로 lecture_teat4.mp4를 입력하여 파이프라인이 정상적으로 동작하는지 확인
